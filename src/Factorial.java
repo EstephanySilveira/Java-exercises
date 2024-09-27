02b0cd2a51177a341bddb2de51ff7e32f1e6077e
Estephany Silveira <estephanysilveira1@gamil.com>
2024-09-27 19:49:12 -0300

    tipos primitivis

--- src/Factorial.java
+++ src/Factorial.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite um número: ");
+        System.out.print("Digite um número: ");
         int num = scanner.nextInt();
 
         int factorial = 1;
