68bae02d2d427ce269e821126aca1f4ae57cdc85
Estephany Silveira <estephanysilveira1@gamil.com>
2024-09-30 09:36:38 -0300

    testando entradas e saídas

--- src/CursoEmVideo/TiposPrimitivos.java
+++ src/CursoEmVideo/TiposPrimitivos.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class TiposPrimitivos {
     public static void main(String[] args) {
+        /*
         Scanner teclado = new Scanner(System.in);
         System.out.print("Digite o nome do aluno: ");
         String nome = teclado.nextLine();
@@ -14,6 +15,19 @@ public class TiposPrimitivos {
         System.out.println("A nota é " + nota);
         System.out.printf("A nota de %s é %.2f ",nome, nota);
         System.out.format("A nota de %s é %.2f ",nome, nota);
+       */
+
+        int idade = 12;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+
+        String v = "30";
+        int idadee = Integer.parseInt(v);
+        System.out.println(idadee);
+
+        String va = "30.5";
+        float ida = Float.parseFloat(va);
+        System.out.printf("%.3f", ida);
     }
 
 
