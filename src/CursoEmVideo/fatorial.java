5b98f4bda69c93e0f889327cdcc08ca183030d17
Estephany Silveira <estephanysilveira1@gamil.com>
2024-10-08 14:30:35 -0300

    fatorial de um número.

---
+++ src/CursoEmVideo/fatorial.java
@@ -0,0 +1,22 @@
+package CursoEmVideo;
+
+import java.util.Scanner;
+
+public class fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Coloque um número: ");
+
+        int n = scanner.nextInt();
+        int f = 1;
+        int c = n;
+
+        while (c >=1) {
+            f *= c;
+            c--;
+        }
+        System.out.println(f);
+
+
+    }
+}
