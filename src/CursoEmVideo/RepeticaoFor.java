4c0163758c94f74f389b66cae5c53276fa7ea6fa
Estephany Silveira <estephanysilveira1@gamil.com>
2024-10-09 16:33:23 -0300

    Repetição de números for

--- src/CursoEmVideo/RepeticaoFor.java
+++ src/CursoEmVideo/RepeticaoFor.java
@@ -1,9 +1,25 @@
 package CursoEmVideo;
 
+import java.util.Scanner;
+
 public class RepeticaoFor {
     public static void main(String[] args) {
-        for(int cc = 1; cc>=100; cc-=10){
-            System.out.println(cc);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Coloque o número de ínicio: ");
+        int i = scanner.nextInt();
+        System.out.print("Coloque o número do fim: ");
+        int f = scanner.nextInt();
+        System.out.print("Coloque o número de que pula: ");
+        int p = scanner.nextInt();
+
+        if (i< f){
+            for (; i<=f; i += p){
+                System.out.println(i);
+            }
+        }else{
+            for(; i>=f; i-=p){
+                System.out.println(i);
+            }
         }
     }
 }
