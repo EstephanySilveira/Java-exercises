fac958efb08d848eac02a4903bcf01471ce992e2
Estephany Silveira <estephanysilveira1@gamil.com>
2024-10-11 15:23:38 -0300

    Introdução métodos

---
+++ src/CursoEmVideo/TesteFuncao01.java
@@ -0,0 +1,14 @@
+package CursoEmVideo;
+
+public class TesteFuncao01 {
+
+    static int soma (int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int sm = soma(5, 2);
+        System.out.println("A soma vale " + sm);
+
+    }
+}
